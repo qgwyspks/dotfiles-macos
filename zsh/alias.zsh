d9cbd1e9329db5169b9b29d26f2cf427b46e2420
54 <102461295+qgwyspks@users.noreply.github.com>
2024-09-02 17:02:08 +0800

    feat: Use single quotes instead of double quotes

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -17,48 +17,48 @@ alias lt='lsd --tree'
 alias mkdir='mkdir -p'
 
 # nvim
-alias vi="nvim"
-alias vim="nvim"
+alias vi='nvim'
+alias vim='nvim'
 
 # fastfetch
-alias neofetch="fastfetch"
+alias neofetch='fastfetch'
 
-alias tt="\time zsh -i -c exit"
+alias tt='\time zsh -i -c exit'
 
 # Conda/Mamba
-alias base="mamba activate base"
-alias pytorch="mamba activate pytorch"
-alias study="mamba activate study"
-alias lab="mamba activate jupyterlab"
-alias dea="mamba deactivate"
+alias base='mamba activate base'
+alias pytorch='mamba activate pytorch'
+alias study='mamba activate study'
+alias lab='mamba activate jupyterlab'
+alias dea='mamba deactivate'
 
 # Python
-alias piptree="pipdeptree"
+alias piptree='pipdeptree'
 
 # LazyDocker
-alias lzd="lazydocker"
+alias lzd='lazydocker'
 
 # git
-alias gs="git status"
-alias gp="git push -u origin main"
-alias ga="git add"
+alias gs='git status'
+alias gp='git push -u origin main'
+alias ga='git add'
 
 # LazyGit
-alias lg="lazygit"
+alias lg='lazygit'
 
 # Proxy
-alias proxy="export https_proxy=http://127.0.0.1:7890;export http_proxy=http://127.0.0.1:7890;export all_proxy=socks5://127.0.0.1:7890"
-alias unproxy="unset https_proxy http_proxy all_proxy"
+alias proxy='export https_proxy=http://127.0.0.1:7890;export http_proxy=http://127.0.0.1:7890;export all_proxy=socks5://127.0.0.1:7890'
+alias unproxy='unset https_proxy http_proxy all_proxy'
 
 # TheFuck
 # eval $(thefuck --alias)
 
 # tmux
-alias t="tmux"
+alias t='tmux'
 
 # asciinema
-alias rec="asciinema rec"
+alias rec='asciinema rec'
 
 # harlequin
-alias sql="harlequin"
+alias sql='harlequin'
 
