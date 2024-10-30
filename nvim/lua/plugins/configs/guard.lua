7f057f811f343c968fcb1f1a3bc8a899d9c4932f
54 <102461295+qgwyspks@users.noreply.github.com>
2024-10-30 17:01:23 +0800

    feat: 添加了python的format和lint

--- nvim/lua/plugins/configs/guard.lua
+++ nvim/lua/plugins/configs/guard.lua
@@ -5,12 +5,27 @@ end
 
 local ft = require('guard.filetype')
 
+-- ft('python'):fmt({
+--                 cmd = 'ruff',
+--                 args = {
+--                     '--line-length=120',
+--                     -- '--config', "\"format.quote-style=\'single\'\"",
+--                     '--config', "format.quote-style='single'",
+--                 },
+--                 stdin = true,
+--             })
+--             :lint('ruff')
+ft('python'):fmt('lsp'):append('ruff'):lint('ruff')
 
 ft('lua'):fmt('lsp')
         :append('stylua')
         -- :lint('selene')
 
-ft('go', 'json'):fmt('lsp')
+ft('go'):fmt('lsp'):append('gofmt')
+
+ft('json'):fmt('lsp')
+
+ft('sh'):lint('shellcheck')
 
 vim.g.guard_config = {
     fmt_on_save = true,
