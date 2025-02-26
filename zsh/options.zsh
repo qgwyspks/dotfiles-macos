26cf5bf73e058acecb8a36bea87ce77154dad12c
54 <1278649519@qq.com>
2025-02-26 20:15:01 +0800

    feat: Remove files about zimfw and add function to set cursor style

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,25 +1,27 @@
-umask 022
-zmodload zsh/zle
-zmodload zsh/zpty
-zmodload zsh/complist
+umask 022  # 设置默认文件创建权限掩码 新文件 -> 644 目录 -> 755
+
+# 加载 zsh 核心模块
+zmodload zsh/zle       # zsh 行编辑器，提供命令行编辑功能
+zmodload zsh/zpty      # 伪终端控制
+zmodload zsh/complist  # 补全列表显示支持
 
 autoload -Uz colors  # 加载颜色支持
-colors  # 启动颜色支持
+colors               # 启动颜色支持
 
 fpath+=~/.zfunc
 autoload -U compinit  # 加载补全系统
 # compinit
 
-
-setopt autocd  # 输入目录名自动 cd
-setopt auto_menu  # 自动显示补全菜单
+setopt autocd            # 输入目录名自动 cd
+setopt auto_menu         # 自动显示补全菜单
 setopt auto_param_slash  # 补全目录时自动添加斜杠
 setopt complete_in_word  # 在单词中间补全
 setopt no_menu_complete  # 不自动选择第一个补全项
 
+# 历史命令存储
 HISTFILE="$HOME/.zsh_history"
-HISTSIZE=1000
-SAVEHIST=2000
+HISTSIZE=1000                 # 内存
+SAVEHIST=2000                 # 文件
 setopt append_history         # 追加历史记录
 setopt share_history          # 所有会话共享历史记录
 setopt inc_append_history     # 立即写入
@@ -31,7 +33,7 @@ setopt hist_expire_dups_first # 先删除重复记录
 setopt hist_save_no_dups      # 不保存重复记录
 setopt hist_reduce_blanks     # 删除历史记录中的多余空格
 
-ZSH_AUTOSUGGEST_USE_ASYNC="true"
+ZSH_AUTOSUGGEST_USE_ASYNC="true"  # 启用 zsh-autosuggestions 插件的异步功能，提升性能
 
 declare -a unopt=(
     FLOWCONTROL   # 禁用流控制
@@ -45,7 +47,43 @@ do
     unsetopt $unopt
 done
 
-bindkey '^A' beginning-of-line  # 绑定 Ctrl+A 到行首
-bindkey '^E' end-of-line  # 绑定 Ctrl+E 到行尾
+bindkey '^A' beginning-of-line        # 绑定 Ctrl+A 到行首
+bindkey '^E' end-of-line              # 绑定 Ctrl+E 到行尾
 bindkey '^p' history-search-backward  # 绑定 Ctrl+P 向上搜索历史命令
-bindkey '^n' history-search-forward  # 绑定 Ctrl+N 向下搜索历史命令
+bindkey '^n' history-search-forward   # 绑定 Ctrl+N 向下搜索历史命令
+
+# 设置光标为竖线形状
+function set_beam_cursor() {
+    echo -ne '\e[5 q'
+}
+
+# 命令行初始化时设置光标形状
+# function zle-line-init() {
+#     set_beam_cursor
+# }
+# zle -N zle-line-init
+
+# 光标形状控制变量
+typeset -g CURSOR_NEEDS_RESET=0
+
+# 命令执行钱检查是否运行 vim/nvim
+function vim_cursor_preexec() {
+    case "$1" in
+        *vim*|*nvim*)
+            CURSOR_NEEDS_RESET=1
+            ;;
+    esac
+}
+
+# 命令执行后检查是否需要重置光标
+function vim_cursor_precmd() {
+    if [ $CURSOR_NEEDS_RESET -eq 1 ]; then
+        set_beam_cursor
+        CURSOR_NEEDS_RESET=0
+    fi
+}
+
+# 初始化钩子函数
+autoload -Uz add-zsh-hook
+add-zsh-hook preexec vim_cursor_preexec
+add-zsh-hook precmd vim_cursor_precmd
