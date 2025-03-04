0f1965e40299713eefcfe5dfa9ed3b300c58db6f
54 <1278649519@qq.com>
2025-03-05 00:58:16 +0800

    feat: Update alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,37 +5,25 @@ alias -g ....='../../..'
 # clear
 alias c='clear'
 
-# lsd -> ls
-alias ls='lsd'
-alias la='lsd -A' # Don't list implied . and ..
-alias ll='lsd -l --header'
-alias llg='lsd -l --header --git'
-alias lla='lsd -lA --header'
-alias lt='lsd --tree'
+if [[ -x $(command -v lsd) ]]; then
+    # lsd -> ls
+    alias ls='lsd'
+    alias la='lsd -A' # Don't list implied . and ..
+    alias ll='lsd -l --header'
+    alias llg='lsd -l --header --git'
+    alias lla='lsd -lA --header'
+    alias lt='lsd --tree'
+else
+    alias ls='ls --color=auto'
+    alias ll='ls -l --color=auto'
+    alias la='ll -a'
+fi
 
 # mkdir
 alias mkdir='mkdir -p'
 
 # nvim
-alias v='nvim'
-alias vi='nvim'
 alias vim='nvim'
-
-# bat
-alias cat='bat --color always --plain'
-
-# fastfetch
-# alias neofetch='fastfetch'
-
-# benchmark
-# zsh 基准测试
-alias benchzsh='for i in $(seq 1 5); do /usr/bin/time /bin/zsh --no-rcs -i -c exit; done'
-# raw zsh 基准测试
-alias benchrawzsh='for i in $(seq 1 20); do /usr/bin/time /bin/zsh --no-rcs -i -c exit; done'
-# zsh 插件启动时间分析测试
-alias benchplugin='zprof | less'
-
-alias tt='\time zsh -i -c exit'
 
 # Conda/Mamba
 alias base='mamba activate base'
@@ -47,3 +35,5 @@ alias dea='mamba deactivate'
 # tmux
 alias t='tmux'
 
+# kubectl
+alias k='kubectl'
