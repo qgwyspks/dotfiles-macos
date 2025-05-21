fd4e395689d042a1be4fa429c3e0895243308507
qgwyspks <1278649519@qq.com>
2025-05-21 14:48:37 +0800

    feat: 修改了一些环境变量配置

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,17 +2,16 @@
 ## PATH & ENV
 ##
 
-export PATH="$HOME/.local/bin:$PATH"
-
 export EDITOR="nvim"
 export VISUAL="nvim"
 
+export PATH="$HOME/.local/bin:$PATH"
 export PATH="$PATH:$HOME/go/bin"
 
 # export PATH="$PATH:$HOME/miniforge3/bin"
 
 # Rust
-export PATH="$PATH:$HOME/.cargo/bin"
+# export PATH="$PATH:$HOME/.cargo/bin"
 
 # sqlite
 #export PATH="/opt/homebrew/opt/sqlite/bin:$PATH"
@@ -23,23 +22,7 @@ export PATH="$PATH:$HOME/.cargo/bin"
 # starship
 export STARSHIP_CONFIG=~/.config/zsh/starship.toml
 
-# eval "$(/Users/wsk/miniforge3/bin/conda shell.zsh hook)"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/wsk/miniforge3/bin/conda' 'shell.zsh' 'hook' 2>/dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/Users/wsk/miniforge3/etc/profile.d/conda.sh" ]; then
-        . "/Users/wsk/miniforge3/etc/profile.d/conda.sh"
-    else
-        export PATH="/Users/wsk/miniforge3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
+eval "$(/Users/wsk/miniforge3/bin/conda shell.zsh hook)"
 if [ -f "/Users/wsk/miniforge3/etc/profile.d/mamba.sh" ]; then
     . "/Users/wsk/miniforge3/etc/profile.d/mamba.sh"
 fi
-# <<< conda initialize <<<
